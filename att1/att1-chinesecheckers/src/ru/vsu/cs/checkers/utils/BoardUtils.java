a72d10f3de89f44fc7321576398556c3509cc719
sevolchenko <se.volchenko@ya.ru>
2021-10-13 11:13:03 +0300

    0.4.2 (BoardUtils is ready to use)

--- att1/att1-chinesecheckers/src/ru/vsu/cs/checkers/utils/BoardUtils.java
+++ att1/att1-chinesecheckers/src/ru/vsu/cs/checkers/utils/BoardUtils.java
@@ -3,6 +3,7 @@ package ru.vsu.cs.checkers.utils;
 import ru.vsu.cs.checkers.piece.Checker;
 import ru.vsu.cs.checkers.structures.graph.Graph;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -69,29 +70,79 @@ public class BoardUtils {
 
                 graph.addAdge(10 * i + 8, 4, 10 * i + 9);
             }
-            if (i > 5) {
-                graph.addAdge(120, i - 6, 10 * i);
-            }
         }
     }
 
     public static void connectSectors(Graph<Checker> graph) {
 
-        graph.addAdge(6, 2, 69);
-        graph.addAdge(6, 3, 110);
+        connectHorizontal(graph, 0, 6);
+        connectHorizontal(graph, 9, 3);
+        connectHorizontal(graph, 7, 8);
+        connectHorizontal(graph, 11, 10);
 
-        graph.addAdge(99, 2, 36);
-        graph.addAdge(80, 3, 36);
+        connectRightDirected(graph, 6, 7);
+        connectRightDirected(graph, 8, 2);
+        connectRightDirected(graph, 10, 9);
+        connectRightDirected(graph, 5, 11);
 
-        for (int i = 7; i < 10; i++) {
-            graph.addAdge(i, 2, 75 - i);
-            graph.addAdge(i, 3, 76 - i);
+        connectLeftDirected(graph, 7, 1);
+        connectLeftDirected(graph, 11, 6);
+        connectLeftDirected(graph, 4, 10);
+        connectLeftDirected(graph, 9, 8);
+    }
 
-            graph.addAdge(89 + i, 2, 46 - i);
-            graph.addAdge(90 + i, 3, 46 - i);
+    private static void connectHorizontal(Graph<Checker> graph, int sectorAbove, int sectorUnder) {
+        graph.addAdge(10 * sectorAbove + 6, 2, 10 * sectorUnder + 9);
+        for (int i = 0; i < 3; i++) {
+            graph.addAdge(sectorAbove * 10 + 7 + i, 3, sectorUnder * 10 + 9 - i);
+            graph.addAdge(sectorAbove * 10 + 7 + i, 2, sectorUnder * 10 + 8 - i);
         }
+    }
 
+    private static void connectRightDirected(Graph<Checker> graph, int sectorLeft, int sectorRight) { // Like /
+        List<Integer> list = new ArrayList<>(List.of(0, 1, 3, 6));
+        int indexLeft = list.get(2);
+        int indexRight = list.get(0);
 
+        graph.addAdge(10 * sectorLeft + 6, 2, 10 * sectorRight);
+        for (int i = 0; i < 3; i++) {
+            graph.addAdge(sectorLeft * 10 + list.get(indexLeft), 1, sectorRight * 10 + list.get(indexRight));
+            graph.addAdge(sectorLeft * 10 + list.get(indexLeft), 2, sectorRight * 10 + list.get(indexRight + 1));
+            indexLeft--;
+            indexRight++;
+        }
+    }
+
+    private static void connectLeftDirected(Graph<Checker> graph, int sectorLeft, int sectorRight) { // Like \
+        List<Integer> list = new ArrayList<>(List.of(0, 2, 5, 9));
+        int indexLeft = list.get(1);
+        int indexRight = list.get(3);
+
+        graph.addAdge(10 * sectorLeft, 1, 10 * sectorRight + 9);
+        for (int i = 0; i < 3; i++) {
+            graph.addAdge(sectorLeft * 10 + list.get(indexLeft), 0, sectorRight * 10 + list.get(indexRight));
+            graph.addAdge(sectorLeft * 10 + list.get(indexLeft), 1, sectorRight * 10 + list.get(indexRight - 1));
+            indexLeft++;
+            indexRight--;
+        }
+    }
+
+    public static void connectWithCenter(Graph<Checker> graph) {
+        graph.addAdge(120, 0, 60);
+        graph.addAdge(120, 1, 76);
+        graph.addAdge(120, 2, 89);
+        graph.addAdge(120, 3, 90);
+        graph.addAdge(120, 4, 106);
+        graph.addAdge(120, 5, 119);
+    }
+
+    public static void connectWithoutFormulas(Graph<Checker> graph) {
+        graph.addAdge(6, 3, 110);
+        graph.addAdge(16, 4, 66);
+        graph.addAdge(20, 5, 76);
+        graph.addAdge(36, 0, 89);
+        graph.addAdge(49, 1, 96);
+        graph.addAdge(50, 2, 109);
     }
 
 }
